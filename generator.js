c55cf450ff11cff172c267b57896f5e2791d1e12
Matthias Fritsche <matthias.fritsche@rwth-aachen.de>
2011-09-13 13:53:42 +0200

    The project is now usable.
    Unfortunately it still lacks a lot in user experience and UI design.

--- generator.js
+++ generator.js
@@ -1,28 +1,28 @@
 var password_hash = {};
-$(document).ready( function(){
-	password_hash = jQuery.getJSON("/flyingfox/password-finder/diceware_german_hash.json", function(password_data){
-			var list = '<ul>';
-			$.each(password_data, function(key, value) {
-				list += '<li>' + key + ' => ' + value + '</li>';
-			});
-		list += '</ul>';
-		password_hash = password_data;
-	});
+var hash_ready = false;
+jQuery.getJSON("/flyingfox/password-finder/diceware_german_hash.json", function(password_data){
+	password_hash = password_data;
+	hash_ready = true;
+	set_password( find_password() );
 });
 
 /*
- * assume thrown_dice is a string and only consists of digits 1-6
- * and the length is dividable by 5
+ * returns the new found password unless the hash or the input isn't ready.
  */
-function find_password(thrown_dice){
-	var i;
-	var digit_pack = [];
-	var password = [];
-	for (i = 0; i < thrown_dice.length; i += 5){
-		digit_pack.push(thrown_dice.slice(i, i + 5) );
+function find_password(){
+	if ( 	hash_ready && input_ready ){
+		var thrown_dice = $("#dice-thrown").val();
+		var i;
+		var digit_pack = [];
+		var password = [];
+		for (i = 0; i < thrown_dice.length; i += 5){
+			digit_pack.push(thrown_dice.slice(i, i + 5) );
+		}
+		for (i = 0; i < digit_pack.length; i ++){
+			password.push( password_hash[digit_pack[i]] );
+		}
+		return password.join(" ");
+	}else {
+		return "";
 	}
-	for (i = 0; i < digit_pack.length; i ++){
-		password.push( password_hash[digit_pack[i]] );
-	}
-	return password.join(" ");
 }
--- password-visuals.js
+++ password-visuals.js
@@ -1,3 +1,4 @@
+var input_ready = false;
 $(document).ready(function() {
 	// things that need to be done when DOM is ready.
 	$("#debug").resizable();
@@ -8,22 +9,48 @@ $(document).ready(function() {
 			value: 0,
 			complete: progressFinished
 	});
-	setProgress();
-	$("#dice-thrown").keyup(setProgress);
+	//setProgress(); //should not be neccessary.
+	$("#dice-thrown").bind("keyup", setProgress);
 	$(".debug-progress").click(function(){
 		$("#debug").append("<br>myval = " + this.value);
 		$("#progress").progressbar("value", parseInt(this.value) );
 	});
+	reset();
 });
 
-function progressFinished(){
-	$(".LCD").addClass("finished");
-	var password = find_password($("#dice-thrown").val());
-	$("#newpassword input").val(password);
-	$("#newpassword").show("highlight", {}, 2000);
+function reset(){
+	$("#dice-thrown").val("");
+	$("#newpassword input").val("");
+	setProgress();
 }
 
-function setProgress(){
+function progressFinished(){
+	if (validate_dice_thrown()){
+		$(".LCD").addClass("finished");
+		input_ready = true;
+		set_password( find_password($("#dice-thrown").val()) );
+	}
+}
+
+function set_password(password){
+	$("#newpassword input").val(password);
+	//only highlight if the password has been set
+	if ( password != "" && password != null){
+		$("#newpassword").show("highlight", {}, 2000);
+	}
+}
+
+function validate_dice_thrown(){
+	var dice = $("#dice-thrown").val();
+	var one_to_six = /^[1-6]*$/;
+	var result = one_to_six.test(dice);
+	if (!result){
+		$("#wrong-text").dialog();
+	}
+	return result;
+}
+
+function setProgress(event){
 	var progress = $("#dice-thrown").val().length;
 	var EASY = 3.0;
 	var HARD = 5.0;
@@ -31,6 +58,12 @@ function setProgress(){
 	var completion = progress / NEEDED_DICE * 100.0;
 	$("#thrown").val(progress);
 	$("#needed").val(NEEDED_DICE);
+	// don't do any validation when backspace was pressed.
+	if ( event != null && event.which == 8){
+		return;
+	}else {
+		validate_dice_thrown();
+	}
 	if ( progress != NEEDED_DICE ){
 		$(".LCD").removeClass("finished");
 	}
