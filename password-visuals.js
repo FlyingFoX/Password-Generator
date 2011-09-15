ff699e6c7f6953520382d7b08b920568a7dae154
Matthias Fritsche <matthias.fritsche@rwth-aachen.de>
2011-09-15 05:05:31 +0200

    Huge UI Update, some bugfixes to the behaviour.
    discarded the .menu() function as it broke and i can't get it back to
    work again. replaced by a css menu, needs more styling.
    todo: replace the .button() css to make them look consistent with the
    new menu.(or the other way around).

--- password-visuals.js
+++ password-visuals.js
@@ -1,26 +1,65 @@
 var input_ready = false;
+var NEEDED_DICE = 15;
+var security_info_visible = false;
 $(document).ready(function() {
 	// things that need to be done when DOM is ready.
-	$("#debug").resizable();
-	$("#debug").draggable();
-	$(":button").button();
-	$("#menu").menu();
+	$(":button, .button").button();
+	$(".dialog").hide();
+	$("#security-info").click(toggleInfo);
+	$("#security-form input").change(adjustSecurity);
 	$("#progress").progressbar({
 			value: 0,
 			complete: progressFinished
 	});
-	//setProgress(); //should not be neccessary.
 	$("#dice-thrown").bind("keyup", setProgress);
-	$(".debug-progress").click(function(){
-		$("#debug").append("<br>myval = " + this.value);
-		$("#progress").progressbar("value", parseInt(this.value) );
-	});
 	reset();
 });
+function toggleInfo(){
+	console.log("toggleInfo: called");
+	if ( security_info_visible ) {
+		console.log("toogleInfo: something is shown, so hide everything.");
+		security_info_visible = false;
+		$("#usability-security-low").hide();
+		$("#usability-security-high").hide();
+	} else{
+		console.log("toggleInfo: nothing shown, now show something.");
+		security_info_visible = true;
+		adjustSecurity();
+	}
+}
 
+function adjustSecurity() {
+	if ($(":checked[name='security']").val() == "low" ){
+		set_needed_dice(15);
+	}else if( $(":checked[name='security']").val() == "high" ){
+		set_needed_dice(25);
+	}
+	if ( security_info_visible ){
+		//something is already shown
+		var duration = "slow";
+		if ( $(":checked[name='security']").val() == "low" ) {
+			$("#usability-security-low").show();
+			$("#usability-security-high").hide();
+			$("#usability-security-low").effect("highlight", {}, duration);
+		} else if ( $(":checked[name='security']").val() == "high" ) {
+			$("#usability-security-low").hide();
+			$("#usability-security-high").show();
+			$("#usability-security-high").effect("highlight", {}, duration);
+		}
+	} else {
+		//nothing is already shown, don't change what is shown
+	}
+}
+
+function set_needed_dice(dice){
+	NEEDED_DICE = dice;
+	$("#needed").val(NEEDED_DICE);
+	$("#dice-thrown").attr("maxlength", dice);
+}
 function reset(){
 	$("#dice-thrown").val("");
 	$("#newpassword input").val("");
+	set_needed_dice(NEEDED_DICE);
 	setProgress();
 }
 
@@ -52,12 +91,8 @@ function validate_dice_thrown(){
 
 function setProgress(event){
 	var progress = $("#dice-thrown").val().length;
-	var EASY = 3.0;
-	var HARD = 5.0;
-	var NEEDED_DICE = 5.0 * EASY;
 	var completion = progress / NEEDED_DICE * 100.0;
 	$("#thrown").val(progress);
-	$("#needed").val(NEEDED_DICE);
 	// don't do any validation when backspace was pressed.
 	if ( event != null && event.which == 8){
 		return;
@@ -67,6 +102,5 @@ function setProgress(event){
 	if ( progress != NEEDED_DICE ){
 		$(".LCD").removeClass("finished");
 	}
-	/*$("#debug").append("<br>setprogress called completion = " + completion);*/
 	$("#progress").progressbar("value", completion );
 }
--- visuals.js
+++ visuals.js
@@ -1,25 +1,5 @@
 $(document).ready(function() {
 	// things that need to be done when DOM is ready.
-	$("#usability-security-high").hide();
-	$(":input[name='security']").change(showSecurity);
-	$("#debug").resizable();
-	$("#debug").draggable();
-	$(":button").button();
-	$("#menu").menu();
+	$(":button, .button").button();
 });
-
-function showSecurity() {
-	var duration = 2000;
-	if ( $(":checked[name='security']").val() == "low" ) {
-		$("#security-form").attr(action, "/flyingfox/password-finder/passwort-einfach.html");
-		$("#usability-security-low").show();
-		$("#usability-security-high").hide();
-		$("#usability-security-low").effect("highlight", {}, duration);
-	} else if ( $(":checked[name='security']").val() == "high" ) {
-		$("#security-form").attr(action, "/flyingfox/password-finder/passwort-sicher.html");
-		$("#usability-security-low").hide();
-		$("#usability-security-high").show();
-		$("#usability-security-high").effect("highlight", {}, duration);
-	}
-}
 
